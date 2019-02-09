07bdeeb788133edb0842613d931f09f3f77c02b5
r4lfl4b <dev@rgalan.es>
2019-02-09 17:33:43 +0000

    refactor styles

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { hot } from 'react-hot-loader';
 import configureStore from './store';
 import App from './components/app';
-import './index.scss';
+import './styles/index.scss';
 
 const HotRootComponent = hot(module)(App);
 
