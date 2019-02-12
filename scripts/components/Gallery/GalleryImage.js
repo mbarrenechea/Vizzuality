3c526469259b4a71880696b759c3de42f72124ee
r4lfl4b <dev@rgalan.es>
2019-02-12 18:44:21 +0000

    Add link to image and <picture> tag

--- scripts/components/Gallery/GalleryImage.js
+++ scripts/components/Gallery/GalleryImage.js
@@ -3,12 +3,19 @@ import PropTypes from 'prop-types';
 
 const GalleryImage = ({ img }) => (
   <div className="card">
-    <div className="card__image">
-      <img className="card__image--img" src={img.urls.small} alt={img.description} />
-    </div>
-    <div className="card__body">
-      <div className="card__body--title">{img.description}</div>
-    </div>
+    <a rel="noopener noreferrer" target="_blank" href={img.urls.full}>
+      <div className="card__image">
+        <picture>
+          <source media="(min-width: 830px)" srcSet={img.urls.regular} />
+          <source media="(min-width: 640px)" srcSet={img.urls.small} />
+          <source media="(min-width: 400px)" srcSet={img.urls.regular} />
+          <img className="card__image--img" src={img.urls.small} alt={img.description} />
+        </picture>
+      </div>
+      <div className="card__body">
+        <div className="card__body--title">{img.description}</div>
+      </div>
+    </a>
   </div>
 );
 
